93b95d7a850bb7312b6bc29e41a8789fcec9c2fe
Nodirbek-Abdulaxadov <nbkabdulaxadov@gmail.com>
2022-02-24 14:10:01 +0500

    boshlandi

--- FoodOrder.WebSite/Controllers/HomeController.cs
+++ FoodOrder.WebSite/Controllers/HomeController.cs
@@ -52,9 +52,28 @@ namespace FoodOrder.WebSite.Controllers
             return View();
         }
 
-        public IActionResult Menu()
+        public async Task<IActionResult> Menu()
         {
-            return View();
+            List<KategoriyaViewModel> viewList1 = new List<KategoriyaViewModel>();
+            List<MahsulotViewModel> viewList2 = new List<MahsulotViewModel>();
+
+            var list1 = await _kategoriyaInterface.GetKategoriyalar();
+            foreach (var item in list1)
+            {
+                viewList1.Add((KategoriyaViewModel)item);
+            }
+            var list2 = await _mahsulotInterface.GetMahsulotlar();
+            foreach (var item in list2)
+            {
+                viewList2.Add((MahsulotViewModel)item);
+            }
+
+            IndexViewModel viewModel = new IndexViewModel()
+            {
+                Kategoriyalar = viewList1,
+                Mahsulotlar = viewList2
+            };
+            return View("Menu", viewModel);
         }
     }
 }
